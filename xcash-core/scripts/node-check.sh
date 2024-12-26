a4486640888df753d8be1082fbef31abf81623f5
farmage <farmage@protonmail.com>
2024-12-26 22:32:55 +0100

    healthcheck fix

--- xcash-core/scripts/node-check.sh
+++ xcash-core/scripts/node-check.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 curl --fail -s -X POST http://xcash-node:18281/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"get_info"}' -H 'Content-Type: application/json' || exit 1
--- xcash-core/scripts/wallet-check.sh
+++ xcash-core/scripts/wallet-check.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 curl --fail -s -X POST http://xcash-wallet:18285/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"get_address"}' -H 'Content-Type: application/json' || exit 1
