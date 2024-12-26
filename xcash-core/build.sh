d469908a2b6024ef556f65cf8adc70e96003d52d
farmage <farmage@protonmail.com>
2024-12-26 12:27:16 +0100

    build script

--- xcash-core/build.sh
+++ xcash-core/build.sh
@@ -1,8 +1,9 @@
-docker buildx build --platform linux/amd64 -t xcashtech/xcash-core:amd64 --load .
-docker buildx build --platform linux/arm64 --provenance false -t xcashtech/xcash-core:arm64  .
-
+docker buildx build --platform linux/amd64 -t xcashtech/xcash-core:amd64 --load --no-cache . && \
+docker buildx build --platform linux/arm64 --provenance false -t xcashtech/xcash-core:arm64 --no-cache . && \
+\
 docker manifest create xcashtech/xcash-core:latest \
   xcashtech/xcash-core:arm64 \
-  xcashtech/xcash-core:amd64
-
-docker manifest push xcashtech/xcash-core:latest
+  xcashtech/xcash-core:amd64 &&
+\
+docker manifest push xcashtech/xcash-core:latest \
+docker push xcashtech/xcash-core:latest
